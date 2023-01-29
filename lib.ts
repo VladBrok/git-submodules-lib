8f3d76b47b7613fe999fd709ba73b8f4dac4b99a
VladBrok <vlad.brok99@gmail.com>
2023-01-29 21:28:22 +0300

    Add sayBye func

--- lib.ts
+++ lib.ts
@@ -1,3 +1,7 @@
 export function sayHello(person?: string) {
   console.log(person ? `Hi, ${person}` : "Hi");
 }
+
+export function sayBye(person?: string) {
+  console.log(person ? `Bye, ${person}` : "Bye");
+}
